374e36d283ee52bc6bdd83b4eb18cdd282b6e466
jackkershaw <hello@jackkershaw.net>
2025-06-07 16:42:03 +0100

    set up and attempt

--- index.js
+++ index.js
@@ -2,31 +2,42 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/api/:time", (req, res) => {
+  const time = req.params.time;
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  // if time is utc, convert to unix
+  if (time.includes("-")) {
+    let utcTime = time;
+    let unixTime = Math.floor(time.getTime() / 1000);
+    res.json({ unix: unixTime, utc: utcTime });
+  }
+  // if time is unix, convert to utc
+  else {
+    let unixTime = time;
+    let utcTime = time; // convert
+    res.json({ unix: unixTime, utc: utcTime });
+  }
 });
-
-
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log(
+    "Your app is listening on port " + listener.address().port
+  );
 });
